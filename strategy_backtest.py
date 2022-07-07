cfcfda2c02594b5691d61ee980ab2994d51423c9
serbazov <serbazov@gmail.com>
2022-07-08 00:43:55 +0300

    добавлена стратегия с нормальным распределением ликвидности

--- strategy_backtest.py
+++ strategy_backtest.py
@@ -6,43 +6,7 @@ import matplotlib.pyplot as plt
 import itertools
 import numpy as np
 
-
-# def plotter(data, ranges):
-#    fee = []
-#    closes = []
-#    amount = []
-#    fees = 0
-#    times = []
-#    for i in range(len(data)):
-#        fees = fees + data[i]["feeUSD"]
-#        closes.append(data[i]["close"])
-#        amount.append(data[i]["amountV"])
-#        fee.append(fees)
-#        times.append((data[i]["periodStartUnix"] - data[0]["periodStartUnix"]) / (3600 * 24))
-
-def plotter(minRange, maxRange, xMin, xMax, fee, closes, amount, times, reinvesting=False):
-    fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(15, 10))
-    ax1.plot(times, fee)
-    ax1.set_title("feeUSD")
-    if closes[0] < 1:
-        ax2.plot(times, [1 / i for i in closes])
-    else:
-        ax2.plot(times, closes)
-    for i in range(len(minRange)):
-        ax2.fill_between(np.arange(xMin[i], xMax[i], 1 / 24), minRange[i], maxRange[i], color='r', alpha=.2)
-    ax2.set_title("closes_price")
-    if reinvesting == False:
-        ax3.plot(times, amount, times, np.array(amount) + np.array(fee))
-    else:
-        ax3.plot(times, amount, times, np.array(amount))
-    ax3.set_title("LP_value")
-    for ax in (ax1, ax2, ax3):
-        ax.grid()
-    # with open("output.txt", "r+") as file1:
-    #     for l in file1.readlines():
-    #         ax2.hlines(float(l), times[0], times[-1], linewidth=1, color='r')
-    plt.show()
-
+from visualization import plotter, plotter_for_distribution
 
 now = int(time.time())
 
@@ -170,7 +134,8 @@ def _2_pos_strategy(percent_itm, width, pool, investmentAmount, endTimestamp=now
                                              protocol=protocol, priceToken=priceToken, period="hourly")
             minBound.append(current_price)
             maxBound.append(current_price + width)
-            xMin.append((prices["periodStartUnix"].values[i - time + 1] - prices["periodStartUnix"].values[0]) / (3600 * 24))
+            xMin.append(
+                (prices["periodStartUnix"].values[i - time + 1] - prices["periodStartUnix"].values[0]) / (3600 * 24))
             xMax.append((prices["periodStartUnix"].values[i] - prices["periodStartUnix"].values[0]) / (3600 * 24))
             top = uniswapStrategyBacktest(pool, investmentAmount / 2,
                                           current_price, current_price + width,
@@ -196,7 +161,6 @@ def _2_pos_strategy(percent_itm, width, pool, investmentAmount, endTimestamp=now
         fee.append(fees)
         times.append((data_top[j]["unixDT"] - prices["periodStartUnix"].values[0]) / (3600 * 24))
     plotter(minBound, maxBound, xMin, xMax, fee, closes, amount, times)
-
 
 
 def _simple_bounds_strategy(width, pool_id, Amount, days, priceToken, endTimestamp=now, protocol=0,
@@ -266,6 +230,57 @@ def _simple_bounds_strategy(width, pool_id, Amount, days, priceToken, endTimesta
             (np.array(end_timestamps) - start_timestamps[0]) / (3600 * 24), fee, closes, amount, times, fee_reinvesting)
 
 
+def normal_distribution_strategy(sigma, pool_id, Amount, days, priceToken, endTimestamp=now, protocol=0,
+                                 fee_reinvesting=False, positions_num=25):
+    prices = getPrices(priceToken)
+    current_price = prices["close"].values[0]
+    start_timestamp = prices["periodStartUnix"].values[0]
+    max_prices_range = 30  # в процентах от данной цены
+    prices_range = np.linspace(current_price * (100 - max_prices_range) / 100,
+                               current_price * (100 + max_prices_range) / 100, num=positions_num)
+
+    relative_liquidity_range = 1 / (sigma * np.sqrt(2 * np.pi)) * np.exp(
+        - (prices_range - current_price) ** 2 / (2 * sigma ** 2))
+    max_bin = max(relative_liquidity_range)
+    liquidity = np.zeros_like(relative_liquidity_range)
+    full_liquidity_multiplier = sum(relative_liquidity_range)
+    for i in range(len(relative_liquidity_range)):
+        liquidity[i] = relative_liquidity_range[i] * Amount / full_liquidity_multiplier
+    # temp = sum(liquidity)
+    # plt.bar(prices_range, liquidity,
+    #         width=(prices_range[1] - prices_range[0]) * 2, color='b')
+    # plt.show()
+    bound_width = (prices_range[1] - prices_range[0]) * 2
+    times = prices["periodStartUnix"].values
+    amount = np.zeros(days * 24 - 1)
+    closes = prices["close"].values
+    fee = np.zeros(days * 24 - 1)
+    fees = np.zeros(days * 24 - 1)
+    low_bound = []
+    high_bound = []
+    end = np.zeros_like(liquidity)
+    start = np.zeros_like(liquidity)
+    for j in range(len(liquidity)):
+        data = uniswapStrategyBacktest(pool_id, liquidity[j], prices_range[j] - bound_width / 2,
+                                       prices_range[j] + bound_width / 2, start_timestamp, days=days,
+                                       priceToken=priceToken)
+        low_bound.append(prices_range[j] - bound_width / 4)
+        high_bound.append(prices_range[j] + bound_width / 4)
+        end[j] = ((endTimestamp - start_timestamp) * liquidity[j] / max(liquidity)) + start_timestamp
+        start[j] = 0.
+        end[j] = end[j] - start_timestamp
+        for q in range(len(data)):
+            amount[q] = amount[q] + data[q]["amountV"]
+            fee[q] = fee[q] + data[q]["feeUSD"]
+    for i in range(len(fee)):
+        times[i] = times[i] - data[0]["unixDT"]
+        fees[i] = sum(fee[:i])
+
+
+    plotter_for_distribution(low_bound, high_bound, start / (3600 * 24), end / (3600 * 24), fees, closes, amount,
+                             times / (3600 * 24))
+
+
 def relative_volume_strategy(width, pool, investmentAmount, endTimestamp=now, days=30, protocol=0, priceToken=0):
     from_date = DateByDaysAgo(days, endTimestamp)
     pass
@@ -303,7 +318,7 @@ if __name__ == "__main__":
 
     # _X_percent_ITM_strategy(95, 10, "0x4e68Ccd3E89f51C3074ca5072bbAC773960dFa36".lower(), investmentAmount, days=days,
     #                         priceToken=1)
-
+    normal_distribution_strategy(100, pool_id, investmentAmount, days, priceToken)
 
 # 0x4e68Ccd3E89f51C3074ca5072bbAC773960dFa36  USDT / WETH 0.3
 # 0x88e6a0c2ddd26feeb64f039a2c41296fcb3f5640  WETH / USDC 0.05
